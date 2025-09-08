4a832ad418243f8c6566d54c10b5edb4a3ac0051
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2025-09-09 02:26:14 +0700

    fix: modify navbar to look more like the design

--- frontend/uniplus/app/components/navbar.jsx
+++ frontend/uniplus/app/components/navbar.jsx
@@ -3,15 +3,34 @@ import Link from 'next/link'
 
 function navbar() {
   return (
-    <nav className='bg-[#333] text-white p-5'>
+    <nav className='bg-white text-black py-8 shadow-md'>
         <div className="container mx-auto">
             <div className= 'flex justify-between items-center'>
-                <div>
-                    <Link href="/">NextAuth</Link>
+                <div className='text-2xl font-bold'>
+                    <Link href="/">🎓UniPLUS</Link>
                 </div>
-                <ul className='flex'>
-                    <li className='mx-2'><Link href="/register">Sign Up</Link></li>
-                    <li className='mx-3'><Link href="/login">Sign In</Link></li>
+                
+                <ul className='flex space-x-10 text-base font-medium'>
+                    <li>
+                        <Link href="/" className='hover:underline'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/browse" className='hover:underline'>Events</Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className='hover:underline'>Page</Link>
+                    </li>
+                </ul>
+
+                <ul className='flex space-x-1 text-base font-medium items-center'>
+                    <li>
+                        <Link href="/register" className='px-4 py-1 inline-block hover:underline text-gray-500'>Sign Up?</Link>
+                    </li>
+                    <li>
+                        <a href='#' className='px-4 py-1 border-2 bg-black border-black text-white rounded-full inline-block font-semibold hover:bg-black hover:text-[#E9E9F4]'>
+                            Sign In
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
