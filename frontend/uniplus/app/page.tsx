7302cfc2b4ac764eeb5144c83f1f22870ef4cb5c
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2025-09-10 11:35:33 +0700

    feat: add about us button in hero section

--- frontend/uniplus/app/page.tsx
+++ frontend/uniplus/app/page.tsx
@@ -31,13 +31,18 @@ export default function page() {
                 Join thousands of students discovering many amazing events, from casual club meetups to major university events.
               </p>
 
-              <a href="#_" className="mt-10 relative inline-flex items-center justify-center p-4 px-12 py-4 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-400 rounded-xl shadow-md group">
-                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-indigo-400 group-hover:translate-x-0 ease">
+              <div className="mt-10 flex flex-col sm:flex-row gap-2 justify-center lg:justify-center">
+                <a href="#_" className="relative inline-flex items-center justify-center p-4 px-12 py-4 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-indigo-400 rounded-xl shadow-md group">
+                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-indigo-400 group-hover:translate-x-0 ease">
                       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                </span>
-                <span className="absolute flex items-center justify-center w-full h-full text-xl text-black font-extrabold transition-all duration-300 transform group-hover:translate-x-full ease">Browse Events</span>
-                <span className="relative invisible">Browse Events</span>
-              </a>
+                  </span>
+                  <span className="absolute flex items-center justify-center w-full h-full text-xl text-black font-extrabold transition-all duration-300 transform group-hover:translate-x-full ease">Browse Events</span>
+                  <span className="relative invisible">Browse Events</span>
+                </a>
+                <a href='#_' className="px-8 py-4 text-indigo-600 font-semibold border-2 border-transparent hover:border-indigo-200 rounded-xl transition-all duration-300 hover:bg-white/50 backdrop-blur-sm">
+                  About Us
+                </a>
+              </div>
             </div>
           </div>
 
