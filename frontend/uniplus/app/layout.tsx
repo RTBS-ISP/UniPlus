8f00e423c7c862f8adf12bcd805ca034599959d9
Zen <sudha.sutaschuto@gmail.com>
2025-10-16 14:46:22 +0700

    update comment

--- frontend/uniplus/app/layout.tsx
+++ frontend/uniplus/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* 👇 Wrap everything in both providers */}
+        {/*  Wrap everything in both providers */}
         <UserProvider>
           <AlertProvider>
             {children}
